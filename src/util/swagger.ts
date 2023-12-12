e8a8311bc3e99d8817de0c4502b66a143328b10f
yeo99 <qw6375@gmail.com>
2023-12-12 22:19:37 +0900

    [ADD] Swagger JWT authorize 기능 추가

--- src/util/swagger.ts
+++ src/util/swagger.ts
@@ -1,9 +1,20 @@
 import { INestApplication } from "@nestjs/common";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import {
+    SwaggerModule,
+    DocumentBuilder,
+    SwaggerCustomOptions
+} from "@nestjs/swagger";
+
+// Swagger 새로고침해도 Token값 유지
+const swaggerCustomOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+        persistAuthorization: true,
+    }
+}
 
 /**
- * Swagger 세팅
- * 
+ * @author yeo99
+ * @description Swagger 세팅
  * @param {INestApplication} app
  */
 export function setupSwagger(app: INestApplication): void {
@@ -11,8 +22,20 @@ export function setupSwagger(app: INestApplication): void {
         .setTitle('NestJS Test API Docs')
         .setDescription('NestJS Test API description')
         .setVersion('1.0.0')
+        /**
+         * JWT 토큰 설정
+         */
+        .addBearerAuth(
+            {
+                type: 'http',
+                scheme: 'bearer',
+                name: 'JWT',
+                in: 'header'
+            },
+            'access-token',
+        )
         .build();
 
     const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api-docs', app, document)
+    SwaggerModule.setup('api-docs', app, document, swaggerCustomOptions)
 }
